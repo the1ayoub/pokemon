7c591e9df94345d8501f93441d4b5b260a5ef5d6
the1ayoub <the1ayoub@gmail.com>
2021-12-27 14:13:25 +0100

    []: fix

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -16,7 +16,7 @@ function useFetch({ url }) {
       .catch((error) => {
         setResponse((prev => ({ ...prev, loading: false, error })));
       });
-  }, []);
+  }, [url]);
   return response;
 }
 
